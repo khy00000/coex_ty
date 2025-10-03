cb6fa47685fecdd3606648827f9fa22d5573891f
khy00000 <lightram23@naver.com>
2025-10-03 13:13:43 +0900

    refactor: cursor 컴포넌트 타입 변환

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -1,46 +1,47 @@
 import React, { useEffect, useState } from "react";
 
-const Cursor = () => {
-  const [position, setPosition] = useState(null);
-  const [hovered, setHovered] = useState(false);
+interface Position {
+  x: number;
+  y: number;
+};
 
-  // 1024 이하에서는 적용 안함
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1024);
+const Cursor: React.FC = () => {
+  const [position, setPosition] = useState<Position | null>(null);
+  const [hovered, setHovered] = useState<boolean>(false);
+  const [isDesktop, setIsDesktop] = useState<boolean>(window.innerWidth > 1024);
 
-  // 현재 해상도 체크
   useEffect(() => {
     const handleResize = () => {
       setIsDesktop(window.innerWidth > 1024);
     };
 
     window.addEventListener("resize", handleResize);
-
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   useEffect(() => {
     if (!isDesktop) return;
 
-    // 마우스 이동 이벤트
-    const move = (e) => {
+    const move = (e: MouseEvent) => {
       requestAnimationFrame(() => {
         setPosition({ x: e.clientX, y: e.clientY });
       });
     };
+
     window.addEventListener("mousemove", move);
 
-    // 호버 대상 이벤트
-    const targets = document.querySelectorAll(
+    const targets = document.querySelectorAll<HTMLElement>(
       ".hero_title_wrap, .hero_slide_right, .here_title_arrow"
     );
+
     const handleMouseEnter = () => setHovered(true);
     const handleMouseLeave = () => setHovered(false);
+
     targets.forEach((el) => {
       el.addEventListener("mouseenter", handleMouseEnter);
       el.addEventListener("mouseleave", handleMouseLeave);
     });
 
-    // cleanup
     return () => {
       window.removeEventListener("mousemove", move);
       targets.forEach((el) => {
@@ -50,7 +51,6 @@ const Cursor = () => {
     };
   }, [isDesktop]);
 
-  // 렌더 조건
   if (!isDesktop || position === null) return null;
 
   return (
